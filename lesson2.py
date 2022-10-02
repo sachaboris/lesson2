728f66e284d963e32547457cbaf6540cec61c07f
sachaboris <sacha.boris1993@gmail.com>
2022-10-03 00:20:28 +0300

    create file lesson2

--- lesson2.py
+++ lesson2.py
@@ -1,1 +1,16 @@
-"""sfdfwef"""
+1 # int
+1.2388 #floot
+"str1" #str
+True # bool
+False # bool
+None # Пустой тип
+per1 = "Hello"
+per2 = 58
+per3 = 256.6
+per4 = None
+per5 = True
+per6 = "Hello"
+per7 = 342.80488
+per8 = True
+per9 = None
+per10 = 256
