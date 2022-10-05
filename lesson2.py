591a9541d6a5ac39b4d94678b5d8ffdd6fd39e68
sachaboris <sacha.boris1993@gmail.com>
2022-10-05 21:57:12 +0300

    create file lesson2

--- lesson2.py
+++ lesson2.py
@@ -1,16 +1,6 @@
-1 # int
-1.2388 #floot
-"str1" #str
-True # bool
-False # bool
-None # Пустой тип
-per1 = "Hello"
-per2 = 58
-per3 = 256.6
-per4 = None
-per5 = True
-per6 = "Hello"
-per7 = 342.80488
-per8 = True
-per9 = None
-per10 = 256
+str1 = "Hello"
+str2 = "12"
+print(type(str2))
+chislo1 = int(str2)
+print(type(chislo1))
+
